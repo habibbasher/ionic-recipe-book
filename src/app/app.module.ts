edd1ff68fd8514a85a32677980a654e9cdf09364
habibbasher <habib0904010@gmail.com>
2018-08-25 11:41:01 +0600

    Tabs page and some other page creation and setting tabs page as root page

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,12 +5,21 @@ import { SplashScreen } from '@ionic-native/splash-screen';
 import { StatusBar } from '@ionic-native/status-bar';
 
 import { MyApp } from './app.component';
-import { HomePage } from '../pages/home/home';
+
+import { TabsPage } from '../pages/tabs/tabs';
+import { RecipePage } from '../pages/recipe/recipe';
+import { RecipesPage } from '../pages/recipes/recipes';
+import { EditRecipePage } from '../pages/edit-recipe/edit-recipe';
+import { ShoppingListPage } from '../pages/shopping-list/shopping-list';
 
 @NgModule({
   declarations: [
     MyApp,
-    HomePage
+    TabsPage,
+    RecipePage,
+    RecipesPage,
+    EditRecipePage,
+    ShoppingListPage
   ],
   imports: [
     BrowserModule,
@@ -19,7 +28,11 @@ import { HomePage } from '../pages/home/home';
   bootstrap: [IonicApp],
   entryComponents: [
     MyApp,
-    HomePage
+    TabsPage,
+    RecipePage,
+    RecipesPage,
+    EditRecipePage,
+    ShoppingListPage
   ],
   providers: [
     StatusBar,
