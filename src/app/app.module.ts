da7b36c13d7239682bab8740c2de1e330b33bc8d
habibbasher <habib0904010@gmail.com>
2018-08-25 14:00:38 +0600

    Model and service creation for shopping list

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,11 +6,15 @@ import { StatusBar } from '@ionic-native/status-bar';
 
 import { MyApp } from './app.component';
 
+// Custom Components
 import { TabsPage } from '../pages/tabs/tabs';
 import { RecipePage } from '../pages/recipe/recipe';
 import { RecipesPage } from '../pages/recipes/recipes';
 import { EditRecipePage } from '../pages/edit-recipe/edit-recipe';
 import { ShoppingListPage } from '../pages/shopping-list/shopping-list';
+
+// Custom Services
+import { ShoppingListService } from '../services/shopping-list';
 
 @NgModule({
   declarations: [
@@ -37,7 +41,8 @@ import { ShoppingListPage } from '../pages/shopping-list/shopping-list';
   providers: [
     StatusBar,
     SplashScreen,
-    {provide: ErrorHandler, useClass: IonicErrorHandler}
+    {provide: ErrorHandler, useClass: IonicErrorHandler},
+    ShoppingListService
   ]
 })
 export class AppModule {}
--- src/pages/shopping-list/shopping-list.ts
+++ src/pages/shopping-list/shopping-list.ts
@@ -1,18 +1,36 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { IonicPage } from 'ionic-angular';
 import { NgForm } from '@angular/forms';
+
+// Custom Models
+import { Ingredient } from '../../models/ingredient';
+
+// Custom Services
+import { ShoppingListService } from '../../services/shopping-list';
 
 @IonicPage()
 @Component({
   selector: 'page-shopping-list',
   templateUrl: 'shopping-list.html',
 })
-export class ShoppingListPage {
+export class ShoppingListPage implements OnInit {
 
+  listItems: Ingredient[];
+
+  constructor(private slService: ShoppingListService) {}
+
+  ngOnInit() {
+    this.loadItems();
+  }
 
   onAddItem(form: NgForm) {
-    console.log('form: ', form);
+    this.slService.addItem(form.value.ingredientName, form.value.ingredientAmount);
+    this.loadItems();
+    form.reset();
+  }
 
+  private loadItems() {
+    this.listItems = this.slService.getItems();
   }
 
 }
